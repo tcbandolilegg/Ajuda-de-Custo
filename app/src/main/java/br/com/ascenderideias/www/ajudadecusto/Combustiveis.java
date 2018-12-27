9a096c95a0ecb020160910a7506544b4e9fd8d9a
Thereza Cristina Bandoli Legg <tcbandolilegg@gmail.com>
2018-12-27 16:16:04 -0200

    Alteração de if para case,  colocação de outro metodo do onlistemClick,  colocado botão sair

--- app/src/main/java/br/com/ascenderideias/www/ajudadecusto/Combustiveis.java
+++ app/src/main/java/br/com/ascenderideias/www/ajudadecusto/Combustiveis.java
@@ -7,7 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.widget.Switch;
+import android.widget.Toast;
 
 
 public class Combustiveis extends AppCompatActivity implements View.OnClickListener {
@@ -15,7 +16,7 @@ public class Combustiveis extends AppCompatActivity implements View.OnClickListe
     EditText alcool;
     EditText diesel;
     EditText gnv;
-    private boolean editmod;
+    private boolean editmod, continua;
 
 
     public float val_gasolina;
@@ -31,10 +32,10 @@ public class Combustiveis extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_combustiveis);
         setTitle("Precificação de Combustíveis");
-        //SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        sharedPref = getSharedPreferences(PREFS_NAME,this.MODE_PRIVATE);
+        sharedPref = getSharedPreferences(PREFS_NAME, this.MODE_PRIVATE);
 
         editmod = false;
+        continua = true;
         gasolina = findViewById(R.id.getGasolina);
         alcool = findViewById(R.id.getAlcool);
         diesel = findViewById(R.id.getDiesel);
@@ -42,43 +43,61 @@ public class Combustiveis extends AppCompatActivity implements View.OnClickListe
 
         Button atualizar = findViewById(R.id.bt_atualiza);
         Button prosseguir = findViewById(R.id.bt_prossegue);
+
         carregarValoresCombustiveis();
+
         atualizar.setOnClickListener(this);
         prosseguir.setOnClickListener(this);
     }
 
     public void onClick(View v) {
         int id = v.getId();
-        if (id == R.id.bt_atualiza) {
-            gasolina.setEnabled(true);
-            alcool.setEnabled(true);
-            diesel.setEnabled(true);
-            gnv.setEnabled(true);
-            editmod = true;
+        switch (v.getId() /*qual foi o Id do botão clicado**/) {
+            case R.id.bt_atualiza:
+                gasolina.setEnabled(true);
+                alcool.setEnabled(true);
+                diesel.setEnabled(true);
+                gnv.setEnabled(true);
+                editmod = true;
+                continua = false;
+                break;
+            case R.id.bt_prossegue:
+                if (editmod) {
+                    salvarValoresCombustiveis();
+                    if (val_gasolina == 0 || val_alcool == 0 || val_diesel == 0 || val_gnv == 0) { //TODO adicionei para não gravar zerado
+                        Toast.makeText(getBaseContext(), "Valor Combustivel não pode ficar zerado", Toast.LENGTH_LONG).show();//TODO adicionei para não gravar zerado
 
-        }
-        if (id == R.id.bt_prossegue) {
-            if (editmod) {
-                salvarValoresCombustiveis();
-              //  editmod = false;
-            }
-            startActivity(new Intent(getBaseContext(), Principal.class));
+                    } else {//TODO adicionei para não gravar zerado
+                        continua = true;//TODO adicionei para não gravar zerado
+                    }//TODO adicionei para não gravar zerado
+
+                }
+                if (continua) {//TODO adicionei para não gravar zerado
+                    startActivity(new Intent(getBaseContext(), Principal.class));
+                }//TODO adicionei para não gravar zerado
 //            finish();
+                break;
+            default:
+                break;
         }
+
+
     }
 
     public void salvarValoresCombustiveis() {
 
-        SharedPreferences.Editor editor = sharedPref.edit();//TODO ver  sobreescrever ???
+        SharedPreferences.Editor editor = sharedPref.edit();
+
         val_gasolina = Float.parseFloat(gasolina.getText().toString());
         val_alcool = Float.parseFloat(alcool.getText().toString());
         val_diesel = Float.parseFloat(diesel.getText().toString());
         val_gnv = Float.parseFloat(gnv.getText().toString());
+
         editor.putFloat("val_gasolina", val_gasolina);
         editor.putFloat("val_alcool", val_alcool);
         editor.putFloat("val_diesel", val_diesel);
         editor.putFloat("val_gnv", val_gnv);
-
+// TODO VALOR NÃO PODEM SER  IGUAL  A ZERO
         editor.apply();
     }
 
--- app/src/main/java/br/com/ascenderideias/www/ajudadecusto/Principal.java
+++ app/src/main/java/br/com/ascenderideias/www/ajudadecusto/Principal.java
@@ -11,22 +11,24 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.content.SharedPreferences;
+import android.widget.Toast;
 
 
 public class Principal extends AppCompatActivity {
     public final String PREFS_NAME = "CombPrecos";
     EditText origem;
     EditText destino;
-    EditText distancia;
+    EditText distancia;//
     Spinner combustivel;
     EditText valor_litro;
     EditText autonomia;
     float ajuda_custo;
-    TextView m_ajuda_custo;
+    EditText m_ajuda_custo;
     public float v_litro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //TODO COMO DESABILITAR O VOLTAR PARA ACTIVITY ANTERIOR? COMBUSTIVEIS
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
         setTitle("Ajuda de Custo");
@@ -35,8 +37,15 @@ public class Principal extends AppCompatActivity {
         distancia = findViewById(R.id.get_distkm);//TODO calcular com MAPS
         valor_litro = findViewById(R.id.get_vallitro);
         autonomia = findViewById(R.id.get_autonomia);
-        m_ajuda_custo = findViewById(R.id.t_ajuda_custo);
-        Button calcular = findViewById(R.id.bt_calcular);
+        m_ajuda_custo = findViewById(R.id.get_ajudacusto);
+
+        // Capture our button from layout
+        Button calcular = (Button) findViewById(R.id.bt_calcular);
+        Button sair = (Button) findViewById(R.id.bt_sair);
+        // Register the onClick listener with the implementation above
+        calcular.setOnClickListener(meusBotoes);
+        sair.setOnClickListener(meusBotoes);
+
 
         final Spinner escolheComustivel = (Spinner) findViewById(R.id.spn_combustivel);
         ArrayAdapter<CharSequence> vinculo =
@@ -48,11 +57,11 @@ public class Principal extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String item = escolheComustivel.getSelectedItem().toString();
                 SharedPreferences sharedPref;
-                sharedPref = getSharedPreferences(PREFS_NAME,0);
+                sharedPref = getSharedPreferences(PREFS_NAME, 0);
 
                 switch (item) {
                     case "Gasolina":
-                        v_litro = sharedPref.getFloat("val_gasolina",0.1f);
+                        v_litro = sharedPref.getFloat("val_gasolina", 0.1f);
                         break;
                     case "Alcool":
                         v_litro = sharedPref.getFloat("val_alcool", 0.0f);
@@ -67,9 +76,6 @@ public class Principal extends AppCompatActivity {
                         v_litro = 0;
                         break;
                 }
-                if (v_litro == 0.0) {
-                    //TODO " não exite valor cadastrado para este combustivel.";
-                }
                 valor_litro.setText(String.valueOf(v_litro));
             }
 
@@ -80,14 +86,29 @@ public class Principal extends AppCompatActivity {
         escolheComustivel.setOnItemSelectedListener(combescolhido);
     }
 
-    public void onClick(View v) {
-        int id = v.getId();
-        if (id == R.id.bt_calcular) {
-            float v1, v2;
-            v1 = Float.parseFloat(distancia.getText().toString());
-            v2 = Float.parseFloat(autonomia.getText().toString());
-            ajuda_custo = 2 * (v1 / v2) * v_litro;
-            m_ajuda_custo.setText(String.valueOf(ajuda_custo));
+    // Create an anonymous implementation of OnClickListener
+    private View.OnClickListener meusBotoes = new View.OnClickListener() {
+        public void onClick(View v) {
+            // do something when the button is clicked
+            // Yes we will handle click here but which button clicked??? We don't know
+
+            // So we will make
+            switch (v.getId() /*to get clicked view id**/) {
+                case R.id.bt_calcular:
+                    Toast.makeText(getBaseContext(), "Calculando", Toast.LENGTH_LONG).show();
+                    float v1, v2;
+                    v1 = Float.parseFloat(distancia.getText().toString());
+                    v2 = Float.parseFloat(autonomia.getText().toString());
+                    ajuda_custo = 2 * (v1 / v2) * v_litro;
+                    m_ajuda_custo.setText(String.valueOf(ajuda_custo));
+                    break;
+                case R.id.bt_sair:
+                    finish();
+                    break;
+
+                default:
+                    break;
+            }
         }
-    }
+    };
 }
